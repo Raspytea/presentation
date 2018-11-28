d157b6a760aa99cd20f9d78af2b9c18558360a80
Roy <roy_oon00@hotmail.com>
2018-11-28 15:53:06 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,17 +7,64 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+        
+          <div class="contentblock">
+
+            <div class="doggoblock">
+
+              <img src="https://i.barkpost.com/wp-content/uploads/2015/02/featmeme.jpg?q=70&fit=crop&crop=entropy&w=808&h=500" />
+
+              <div> 
+                    Doggo ipsum very good spot shoober clouds h*ck very good spot, long doggo he made many woofs. 
+                    Waggy wags big ol long woofer shoober, extremely cuuuuuute.  You are doing me the shock borkf heckin angery 
+                    woofer shoob, length boy wow such tempt borkdrive, blop pats. pupper wrinkler very taste wow. 
+                    The neighborhood pupper heck mlem heck, what a nice floof. Bork you are doing me a frighten wow such tempt 
+                    blop he made many woofs, the neighborhood pupper I am bekom fat noodle horse long woofer lotsa pats, woofer 
+                    mlem you are doin me a concern.
+              </div>
+
+            </div>  
+
+            <div class="doggoblock">
+
+              <img src= "https://i.imgur.com/tPWUaWg.gif"/>
+             
+              <div>
+                    Stop it fren borkf big ol pupper many pats, thicc. The neighborhood pupper most angery pupper I have ever seen
+                     wow such tempt extremely cuuuuuute bork you are doin me a concern, waggy wags pupperino much ruin diet long woofer. 
+                     Very jealous pupper smol borking doggo with a long snoot for pats noodle horse you are doing me the shock, thicc. 
+             
+              </div> 
+
+            </div>
+             
+            <div class="doggoblock">
+              <img src= "https://barkpost.com/wp-content/uploads/2015/02/48ACD.png"/>
+              <div>
+                    Heckin good boys and girls many pats pupper dat tungg tho wow very biscit most angery pupper I have ever seen adorable 
+                    doggo floofs, I am bekom fat pats very hand that feed shibe waggy wags big ol vvv.  ur givin me a spook corgo. Smol woofer 
+                    borkf porgo much ruin diet, the neighborhood pupper extremely cuuuuuute big ol pupper h*ck borkdrive, shoober long woofer 
+                    wow very biscit. lotsa pats big ol. Length boy aqua doggo borkdrive, borking doggo.
+              </div> 
+              <img src="https://i.kym-cdn.com/photos/images/original/001/156/712/f03.jpg"/>
+            </div>
+            <div class="doggoblock">  
+              <div>
+                    Super chub big ol pupper clouds, heckin. Heckin good boys shooberino I am bekom fat heckin angery woofer, long doggo. 
+                    Bork stop it fren smol wow such tempt such treat, pupperino doge clouds big ol, very jealous pupper long doggo vvv. Much 
+                    ruin diet borkf mlem noodle horse, borkdrive boofers. Heckin h*ck fat boi extremely cuuuuuute, he made many woofs mlem. 
+                    Waggy wags you are doing me the shock thicc blop long woofer super chub dat tungg tho ur givin me a spook you are doing me a frighten.
+              </div>
+            </div>  
+          </div>
+          
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://images-cdn.9gag.com/photo/aGeb22G_700b.jpg"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            CLICK FOR FRUITS
           </a>
         </header>
       </div>
